ce0960909b22bb56696bd4356f10f82cd5906ebd
vishnupriya-kamaraj <vishnupriya.k.2024.it@rajalakshmi.edu.in>
2025-12-31 20:41:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+function showSection(id) {
+  document.querySelectorAll(".section").forEach(sec => {
+    sec.classList.remove("active");
+  });
+
+  document.getElementById(id).classList.add("active");
+
+  document.getElementById("navMenu").classList.remove("show");
+}
+
+function toggleMenu() {
+  document.getElementById("navMenu").classList.toggle("show");
 }
